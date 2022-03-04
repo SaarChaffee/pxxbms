45801935e4da2233832f8d7ea9d4cadf3413f0f4
SaarChaffee <1240148332@qq.com>
2022-03-04 08:45:14 +0800

    GoodType entity

--- src/main/java/com/chaffee/entity/GoodType.java
+++ src/main/java/com/chaffee/entity/GoodType.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * @author SaarChaffee
  * @since 2022-03-02 16:29:23
  */
-public class GoodType{
+public class GoodType {
   /**
    * 主键ID
    */
@@ -16,28 +16,95 @@ public class GoodType{
   /**
    * 商品类别编号
    */
-  private Integer typecode;
+  private Integer typeCode;
   /**
    * 商品类别名称
    */
-  private String typename;
+  private String typeName;
   /**
    * 创建者
    */
-  private Integer createdby;
+  private Integer createdBy;
   /**
    * 创建时间
    */
-  private Date creationdate;
+  private Date creationDate;
   /**
    * 修改者
    */
-  private Integer modifyby;
+  private Integer modifyBy;
   /**
    * 修改时间
    */
-  private Date modifydate;
+  private Date modifyDate;
   
+  public Integer getId() {
+    return id;
+  }
   
+  public void setId( Integer id ) {
+    this.id = id;
+  }
+  
+  public Integer getTypeCode() {
+    return typeCode;
+  }
+  
+  public void setTypeCode( Integer typeCode ) {
+    this.typeCode = typeCode;
+  }
+  
+  public String getTypeName() {
+    return typeName;
+  }
+  
+  public void setTypeName( String typeName ) {
+    this.typeName = typeName;
+  }
+  
+  public Integer getCreatedBy() {
+    return createdBy;
+  }
+  
+  public void setCreatedBy( Integer createdBy ) {
+    this.createdBy = createdBy;
+  }
+  
+  public Date getCreationDate() {
+    return creationDate;
+  }
+  
+  public void setCreationDate( Date creationDate ) {
+    this.creationDate = creationDate;
+  }
+  
+  public Integer getModifyBy() {
+    return modifyBy;
+  }
+  
+  public void setModifyBy( Integer modifyBy ) {
+    this.modifyBy = modifyBy;
+  }
+  
+  public Date getModifyDate() {
+    return modifyDate;
+  }
+  
+  public void setModifyDate( Date modifyDate ) {
+    this.modifyDate = modifyDate;
+  }
+  
+  @Override
+  public String toString() {
+    return "GoodType{" +
+        "id=" + id +
+        ", typeCode=" + typeCode +
+        ", typeName='" + typeName + '\'' +
+        ", createdBy=" + createdBy +
+        ", creationDate=" + creationDate +
+        ", modifyBy=" + modifyBy +
+        ", modifyDate=" + modifyDate +
+        '}';
+  }
 }
 
