0b817fbd60eef3fe56aa3d0688261b77c0b3dfa1
SaarChaffee <1240148332@qq.com>
2022-03-04 09:53:57 +0800

    entities

--- src/main/java/com/chaffee/entity/Bill.java
+++ src/main/java/com/chaffee/entity/Bill.java
@@ -16,15 +16,11 @@ public class Bill {
   /**
    * 订单号
    */
-  private String billcode;
-  /**
-   * 商品名
-   */
-  private String goodname;
+  private String billCode;
   /**
    * 商品编号
    */
-  private String goodcode;
+  private String goodCode;
   /**
    * 购买数量
    */
@@ -32,15 +28,15 @@ public class Bill {
   /**
    * 商品单价
    */
-  private Integer goodprice;
+  private Integer goodPrice;
   /**
    * 总价
    */
-  private Integer totalprice;
+  private Integer totalPrice;
   /**
    * 顾客编号
    */
-  private String customercode;
+  private String customerCode;
   /**
    * 配送地址
    */
@@ -48,32 +44,212 @@ public class Bill {
   /**
    * 下单时间
    */
-  private Date billtime;
+  private Date billTime;
   /**
    * 支付方式
    */
-  private Integer paymentmethod;
+  private Integer paymentMethod;
   /**
    * 配送时间
    */
-  private Date deliverytime;
+  private Date deliveryTime;
   /**
    * 创建者
    */
-  private Integer createdby;
+  private Integer createdBy;
   /**
    * 创建时间
    */
-  private Date creationdate;
+  private Date creationDate;
   /**
    * 修改者
    */
-  private Integer modifyby;
+  private Integer modifyBy;
   /**
    * 修改时间
    */
-  private Date modifydate;
+  private Date modifyDate;
+  
+  /**
+   * 顾客姓名
+   */
+  private String customerName;
+  /**
+   * 商品名
+   */
+  private String goodName;
+  /**
+   * 支付方式
+   */
+  private String paymentMethodName;
   
   
+  public Integer getId() {
+    return id;
+  }
+  
+  public void setId( Integer id ) {
+    this.id = id;
+  }
+  
+  public String getBillCode() {
+    return billCode;
+  }
+  
+  public void setBillCode( String billCode ) {
+    this.billCode = billCode;
+  }
+  
+  public String getGoodCode() {
+    return goodCode;
+  }
+  
+  public void setGoodCode( String goodCode ) {
+    this.goodCode = goodCode;
+  }
+  
+  public Integer getQuantity() {
+    return quantity;
+  }
+  
+  public void setQuantity( Integer quantity ) {
+    this.quantity = quantity;
+  }
+  
+  public Integer getGoodPrice() {
+    return goodPrice;
+  }
+  
+  public void setGoodPrice( Integer goodPrice ) {
+    this.goodPrice = goodPrice;
+  }
+  
+  public Integer getTotalPrice() {
+    return totalPrice;
+  }
+  
+  public void setTotalPrice( Integer totalPrice ) {
+    this.totalPrice = totalPrice;
+  }
+  
+  public String getCustomerCode() {
+    return customerCode;
+  }
+  
+  public void setCustomerCode( String customerCode ) {
+    this.customerCode = customerCode;
+  }
+  
+  public String getAddress() {
+    return address;
+  }
+  
+  public void setAddress( String address ) {
+    this.address = address;
+  }
+  
+  public Date getBillTime() {
+    return billTime;
+  }
+  
+  public void setBillTime( Date billTime ) {
+    this.billTime = billTime;
+  }
+  
+  public Integer getPaymentMethod() {
+    return paymentMethod;
+  }
+  
+  public void setPaymentMethod( Integer paymentMethod ) {
+    this.paymentMethod = paymentMethod;
+  }
+  
+  public Date getDeliveryTime() {
+    return deliveryTime;
+  }
+  
+  public void setDeliveryTime( Date deliveryTime ) {
+    this.deliveryTime = deliveryTime;
+  }
+  
+  public Integer getCreatedBy() {
+    return createdBy;
+  }
+  
+  public void setCreatedBy( Integer createdBy ) {
+    this.createdBy = createdBy;
+  }
+  
+  public Date getCreationDate() {
+    return creationDate;
+  }
+  
+  public void setCreationDate( Date creationDate ) {
+    this.creationDate = creationDate;
+  }
+  
+  public Integer getModifyBy() {
+    return modifyBy;
+  }
+  
+  public void setModifyBy( Integer modifyBy ) {
+    this.modifyBy = modifyBy;
+  }
+  
+  public Date getModifyDate() {
+    return modifyDate;
+  }
+  
+  public void setModifyDate( Date modifyDate ) {
+    this.modifyDate = modifyDate;
+  }
+  
+  public String getCustomerName() {
+    return customerName;
+  }
+  
+  public void setCustomerName( String customerName ) {
+    this.customerName = customerName;
+  }
+  
+  public String getGoodName() {
+    return goodName;
+  }
+  
+  public void setGoodName( String goodName ) {
+    this.goodName = goodName;
+  }
+  
+  public String getPaymentMethodName() {
+    return paymentMethodName;
+  }
+  
+  public void setPaymentMethodName( String paymentMethodName ) {
+    this.paymentMethodName = paymentMethodName;
+  }
+  
+  @Override
+  public String toString() {
+    return "Bill{" +
+        "id=" + id +
+        ", billCode='" + billCode + '\'' +
+        ", goodCode='" + goodCode + '\'' +
+        ", quantity=" + quantity +
+        ", goodPrice=" + goodPrice +
+        ", totalPrice=" + totalPrice +
+        ", customerCode='" + customerCode + '\'' +
+        ", address='" + address + '\'' +
+        ", billTime=" + billTime +
+        ", paymentMethod=" + paymentMethod +
+        ", deliveryTime=" + deliveryTime +
+        ", createdBy=" + createdBy +
+        ", creationDate=" + creationDate +
+        ", modifyBy=" + modifyBy +
+        ", modifyDate=" + modifyDate +
+        ", customerName='" + customerName + '\'' +
+        ", goodName='" + goodName + '\'' +
+        ", paymentMethodName='" + paymentMethodName + '\'' +
+        '}';
+  }
 }
 
--- src/main/java/com/chaffee/entity/PaymentMethod.java
+++ src/main/java/com/chaffee/entity/PaymentMethod.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * @author SaarChaffee
  * @since 2022-03-02 16:29:23
  */
-public class PaymentMethod{
+public class PaymentMethod {
   /**
    * 主键ID
    */
@@ -38,6 +38,73 @@ public class PaymentMethod{
    */
   private Date modifyDate;
   
+  public Integer getId() {
+    return id;
+  }
   
+  public void setId( Integer id ) {
+    this.id = id;
+  }
+  
+  public Integer getTypeCode() {
+    return typeCode;
+  }
+  
+  public void setTypeCode( Integer typeCode ) {
+    this.typeCode = typeCode;
+  }
+  
+  public String getTypeName() {
+    return typeName;
+  }
+  
+  public void setTypeName( String typeName ) {
+    this.typeName = typeName;
+  }
+  
+  public Integer getCreatedBy() {
+    return createdBy;
+  }
+  
+  public void setCreatedBy( Integer createdBy ) {
+    this.createdBy = createdBy;
+  }
+  
+  public Date getCreationDate() {
+    return creationDate;
+  }
+  
+  public void setCreationDate( Date creationDate ) {
+    this.creationDate = creationDate;
+  }
+  
+  public Integer getModifyBy() {
+    return modifyBy;
+  }
+  
+  public void setModifyBy( Integer modifyBy ) {
+    this.modifyBy = modifyBy;
+  }
+  
+  public Date getModifyDate() {
+    return modifyDate;
+  }
+  
+  public void setModifyDate( Date modifyDate ) {
+    this.modifyDate = modifyDate;
+  }
+  
+  @Override
+  public String toString() {
+    return "PaymentMethod{" +
+        "id=" + id +
+        ", typeCode=" + typeCode +
+        ", typeName='" + typeName + '\'' +
+        ", createdBy=" + createdBy +
+        ", creationDate=" + creationDate +
+        ", modifyBy=" + modifyBy +
+        ", modifyDate=" + modifyDate +
+        '}';
+  }
 }
 
