69c00424c3661228357b63eb1bf0827b18b0b31a
SaarChaffee <1240148332@qq.com>
2022-03-08 16:55:55 +0800

    add charset filter

---
+++ src/main/java/com/chaffee/filter/CharacterEncodingFilter.java
@@ -0,0 +1,28 @@
+package com.chaffee.filter;
+
+import javax.servlet.*;
+
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+  @Override
+  public void init( FilterConfig filterConfig ) throws ServletException {
+    Filter.super.init( filterConfig );
+  }
+  
+  @Override
+  public void doFilter( ServletRequest servletRequest, ServletResponse servletResponse,
+                        FilterChain filterChain ) throws IOException, ServletException {
+    
+    servletRequest.setCharacterEncoding( "utf-8" );
+    servletResponse.setCharacterEncoding( "utf-8" );
+    
+    filterChain.doFilter( servletRequest, servletResponse );
+    
+  }
+  
+  @Override
+  public void destroy() {
+    Filter.super.destroy();
+  }
+}
