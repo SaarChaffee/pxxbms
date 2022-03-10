bce7dbed3aabad7360774a9b988ab901fbb29435
SaarChaffee <1240148332@qq.com>
2022-03-10 21:34:16 +0800

    init user role service

---
+++ src/main/java/com/chaffee/service/user/UserRoleService.java
@@ -0,0 +1,10 @@
+/**
+ * @Name: pxxbms
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/3/10
+ */
+package com.chaffee.service.user;
+
+public interface UserRoleService {
+}
