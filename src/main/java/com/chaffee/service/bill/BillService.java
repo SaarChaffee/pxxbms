e7404bcb6e2e4b3a1d97937df06fc2c5a3e7e94b
SaarChaffee <1240148332@qq.com>
2022-03-11 16:15:37 +0800

    bill service

---
+++ src/main/java/com/chaffee/service/bill/BillService.java
@@ -0,0 +1,17 @@
+/**
+ * @Name: pxxbms
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/3/11
+ */
+package com.chaffee.service.bill;
+
+import com.chaffee.entity.Bill;
+
+import java.util.List;
+
+public interface BillService {
+  public List<Bill> getBillList( String goodName, String customerName, int paymentMethod,
+                                 int currentPageNo,
+                                 int pageSize);
+}
---
+++ src/main/java/com/chaffee/service/bill/BillServiceImpl.java
@@ -0,0 +1,44 @@
+/**
+ * @Name: pxxbms
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/3/11
+ */
+package com.chaffee.service.bill;
+
+import com.chaffee.dao.DaoUtils;
+import com.chaffee.dao.bill.BillDao;
+import com.chaffee.dao.bill.BillDaoImpl;
+import com.chaffee.entity.Bill;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class BillServiceImpl implements BillService {
+  private final BillDao billDao;
+  
+  public BillServiceImpl() {
+    billDao = new BillDaoImpl();
+  }
+  
+  
+  @Override
+  public List<Bill> getBillList( String goodName, String customerName, int paymentMethod,
+                                 int currentPageNo,
+                                 int pageSize ) {
+    Connection connection = null;
+    List<Bill> bills = null;
+    
+    try{
+      connection = DaoUtils.getConnection();
+      bills = billDao.getBillList( connection, goodName, customerName, paymentMethod, currentPageNo, pageSize );
+    }catch( SQLException e ){
+      e.printStackTrace();
+    }finally{
+      DaoUtils.close( connection, null, null );
+    }
+    
+    return bills;
+  }
+}
