c61707f0a149752ccca81bf8cf1ae84f546161bd
SaarChaffee <1240148332@qq.com>
2022-03-12 14:15:45 +0800

    init good service

---
+++ src/main/java/com/chaffee/service/good/GoodTypeServiceImpl.java
@@ -0,0 +1,10 @@
+/**
+ * @Name: pxxbms
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/3/12
+ */
+package com.chaffee.service.good;
+
+public class GoodTypeServiceImpl implements GoodTypeService {
+}
