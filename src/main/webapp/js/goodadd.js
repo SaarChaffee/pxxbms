e5855cd7e8ff4678f1234700034b42d49d3af054
SaarChaffee <1240148332@qq.com>
2022-03-13 15:52:10 +0800

    good add page

---
+++ src/main/webapp/js/goodadd.js
@@ -0,0 +1,166 @@
+﻿var goodCode = null;
+var goodName = null;
+var ownerCode = null;
+var ownerId = null;
+var ownerName = null;
+var inventory = null;
+var goodType = null;
+var addBtn = null;
+var backBtn = null;
+
+function priceReg(value) {
+  value = value.replace(/[^\d.]/g, "");  //清除“数字”和“.”以外的字符
+  value = value.replace(/^\./g, "");  //验证第一个字符是数字而不是.
+  value = value.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的.
+  value = value.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");//去掉特殊符号￥
+  if (value.indexOf(".") > 0) {
+    value = value.substring(0, value.indexOf(".") + 3);
+  }
+  return value;
+}
+
+
+$(function () {
+  goodCode = $("#goodCode");
+  goodName = $("#goodName");
+  ownerCode = $("#ownerCode");
+  ownerId = $("#ownerId");
+  ownerName = $("#ownerName");
+  inventory = $("#inventory");
+  goodType = $("#goodType");
+  addBtn = $("#add");
+  backBtn = $("#back");
+  
+  //初始化的时候，要把所有的提示信息变为：* 以提示必填项，更灵活，不要写在页面上
+  goodCode.next().html("*");
+  goodName.next().html("*");
+  ownerCode.next().html("*");
+  inventory.next().html("*");
+  goodType.next().html("*");
+  
+  $.ajax({
+    type: "GET",//请求类型
+    url: path + "/jsp/good.do",//请求的url
+    data: {method: "gettypelist"},//请求参数
+    dataType: "json",//ajax接口（请求url）返回的数据类型
+    success: function (data) {//data：返回数据（json对象）
+      if (data != null) {
+        $("select").html("");//通过标签选择器，得到select标签，适用于页面里只有一个select
+        var options = "<option value=\"0\">请选择</option>";
+        for (var i = 0; i < data.length; i++) {
+          //alert(data[i].id);
+          //alert(data[i].typeName);
+          options += "<option value=\"" + data[i].id + "\">" + data[i].typeName + "</option>";
+        }
+        $("select").html(options);
+      }
+    },
+    error: function (data) {//当访问时候，404，500 等非200的错误状态码
+      validateTip(goodType.next(), {"color": "red"}, imgNo + " 获取商品类型列表error", false);
+    }
+  });
+  /*
+   * 验证
+   * 失焦\获焦
+   * jquery的方法传递
+   */
+  goodCode.on("blur", function () {
+    if (goodCode.val() != null && goodCode.val() != "") {
+      validateTip(goodCode.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(goodCode.next(), {"color": "red"}, imgNo + " 订单号不能为空，请重新输入", false);
+    }
+  }).on("focus", function () {
+    //显示友情提示
+    validateTip(goodCode.next(), {"color": "#666666"}, "* 请输入订单号", false);
+  }).focus();
+  
+  goodName.on("focus", function () {
+    validateTip(goodName.next(), {"color": "#666666"}, "* 请输入商品名称", false);
+  }).on("blur", function () {
+    if (goodName.val() != null && goodName.val() != "") {
+      validateTip(goodName.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(goodName.next(), {"color": "red"}, imgNo + " 商品名称不能为空，请重新输入", false);
+    }
+    
+  });
+  
+  ownerCode.on("focus", function () {
+    validateTip(ownerCode.next(), {"color": "#666666"}, "* 请输入商家账户", false);
+  }).on("blur", function () {
+    if (ownerCode.val() != null && ownerCode.val() != "") {
+      $.ajax({
+        type: "GET",
+        url: path + "/jsp/user.do",
+        data: {method: "getUserByCode", userCode: ownerCode.val()},
+        dataType: "json",
+        success: function (data) {
+          if (data.flag) {
+            ownerId.val(data.uid);
+            ownerName.val(data.uname);
+            validateTip(ownerCode.next(), {"color": "green"}, imgYes, true);
+          } else {
+            validateTip(ownerCode.next(), {"color": "red"}, imgNo + " 商家姓名错误或不存在，请重新输入", false);
+          }
+        },
+        error: function (data) {
+          validateTip(ownerCode.next(), {"color": "red"}, imgNo + " 获取商家姓名error", false);
+        }
+      })
+    } else {
+      validateTip(ownerCode.next(), {"color": "red"}, imgNo + " 商家账户不能为空，请重新输入", false);
+    }
+  });
+  
+  goodType.on("focus", function () {
+    validateTip(goodType.next(), {"color": "#666666"}, "* 请选择商品类型", false);
+  }).on("blur", function () {
+    if (goodType.val() != null && goodType.val() != "" && goodType.val() != 0) {
+      validateTip(goodType.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(goodType.next(), {"color": "red"}, imgNo + " 商品类型不能为空，请选择", false);
+    }
+    
+  });
+  
+  inventory.on("focus", function () {
+    validateTip(inventory.next(), {"color": "#666666"}, "* 请输入正整数", false);
+  }).on("blur", function () {
+    if (this.value != null && inventory.val() != null) {
+      validateTip(inventory.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(inventory.next(), {"color": "red"}, imgNo + " 库存不能为空，请选择", false);
+      
+    }
+  });
+  
+  
+  addBtn.on("click", function () {
+    if (goodCode.attr("validateStatus") != "true") {
+      goodCode.blur();
+    } else if (goodName.attr("validateStatus") != "true") {
+      goodName.blur();
+    } else if (ownerCode.attr("validateStatus") != "true") {
+      ownerCode.blur();
+    } else if (goodType.attr("validateStatus") != "true") {
+      goodType.blur();
+    } else {
+      if (confirm("是否确认提交数据")) {
+        $("#billForm").submit();
+      }
+    }
+  });
+  
+  backBtn.on("click", function () {
+    if (referer != undefined
+        && null != referer
+        && "" != referer
+        && "null" != referer
+        && referer.length > 4) {
+      window.location.href = referer;
+    } else {
+      history.back(-1);
+    }
+  });
+});
--- src/main/webapp/jsp/goodadd.jsp
+++ src/main/webapp/jsp/goodadd.jsp
@@ -1,16 +1,54 @@
-<%--
-  Name: pxxbms
-  Author: SaarChaffee
-  Code: utf-8
-  Date: Created in 2022 2022/3/13
---%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<%@include file="/jsp/common/head.jsp" %>
 
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
-Body
-</body>
-</html>
+<div class="right">
+    <div class="location">
+        <strong>你现在所在的位置是:</strong>
+        <span>商品管理页面 >> 商品添加页面</span>
+    </div>
+    <div class="providerAdd">
+        <form id="goodForm" name="goodForm" method="post" action="${pageContext.request.contextPath }/jsp/good.do">
+            <!--div的class 为error是验证错误，ok是验证成功-->
+            <input type="hidden" name="method" value="add">
+            <div class="">
+                <label for="goodCode">商品编号：</label>
+                <input type="text" name="goodCode" class="text" id="goodCode" value="">
+                <!-- 放置提示信息 -->
+                <font color="red"></font>
+            </div>
+            <div>
+                <label for="goodName">商品名称：</label>
+                <input type="text" name="goodName" id="goodName" value="">
+                <font color="red"></font>
+            </div>
+            <div>
+                <label for="inventory">商品库存：</label>
+                <input type="text" name="inventory" id="inventory" value="">
+                <font color="red"></font>
+            </div>
+            <div>
+                <label for="ownerCode">商家账号：</label>
+                <input type="text" name="ownerCode" id="ownerCode" value="">
+                <font color="red"></font>
+            </div>
+            <div>
+                <label for="ownerName">商家姓名：</label>
+                <input type="text" name="ownerName" id="ownerName" value="" readonly="readonly">
+                <input type="hidden" name="ownerId" id="ownerId" value="">
+            </div>
+            <div>
+                <label for="goodType">商品类型：</label>
+                <select name="goodType" id="goodType"></select>
+                <font color="red"></font>
+            </div>
+            <div class="providerAddBtn">
+                <input type="button" name="add" id="add" value="保存">
+                <input type="button" id="back" name="back" value="返回">
+            </div>
+        </form>
+    </div>
+</div>
+</section>
+<%@include file="/jsp/common/foot.jsp" %>
+<script type="text/javascript" src="${pageContext.request.contextPath }/js/goodadd.js"></script>
