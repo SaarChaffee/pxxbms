1729fbaafa8f4a71fca653a49c6e516a122b9d60
SaarChaffee <1240148332@qq.com>
2022-03-13 09:26:42 +0800

    good modify page

---
+++ src/main/webapp/js/goodmodify.js
@@ -0,0 +1,122 @@
+var goodName = null;
+var goodCode = null;
+var inventory = null;
+var goodType = null;
+var saveBtn = null;
+var backBtn = null;
+
+$(function () {
+  goodName = $("#goodName");
+  goodCode = $("#goodCode");
+  inventory = $("#inventory");
+  goodType = $("#goodType");
+  saveBtn = $("#save");
+  backBtn = $("#back");
+  
+  goodName.next().html("*");
+  goodCode.next().html("*");
+  inventory.next().html("*");
+  goodType.next().html("*");
+  
+  
+  $.ajax({
+    type: "GET",//请求类型
+    url: path + "/jsp/good.do",//请求的url
+    data: {method: "gettypelist"},//请求参数
+    dataType: "json",//ajax接口（请求url）返回的数据类型
+    success: function (data) {//data：返回数据（json对象）
+      if (data != null) {
+        var tid = $("#tid").val();
+        goodType.html("");
+        var options = "<option value=\"0\">请选择</option>";
+        for (var i = 0; i < data.length; i++) {
+          //alert(data[i].id);
+          //alert(data[i].typeName);
+          if (tid != null && tid != undefined && data[i].id == tid) {
+            options += "<option selected=\"selected\" value=\"" + data[i].id + "\" >" + data[i].typeName + "</option>";
+          } else {
+            options += "<option value=\"" + data[i].id + "\" >" + data[i].typeName + "</option>";
+          }
+          
+        }
+        goodType.html(options);
+      }
+    },
+    error: function (data) {//当访问时候，404，500 等非200的错误状态码
+      validateTip(goodType.next(), {"color": "red"}, imgNo + " 获取商品类型列表error", false);
+    }
+  });
+  
+  
+  goodName.on("focus", function () {
+    validateTip(goodName.next(), {"color": "#666666"}, "* 商品名长度必须是大于1小于10的字符", false);
+  }).on("blur", function () {
+    if (goodName.val() != null && goodName.val().length > 1
+        && goodName.val().length < 10) {
+      validateTip(goodName.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(goodName.next(), {"color": "red"}, imgNo + " 商品名输入的不符合规范，请重新输入", false);
+    }
+    
+  });
+  
+  goodCode.on("focus", function () {
+    validateTip(goodCode.next(), {"color": "#666666"}, "* 商品编号度必须是大于1小于10的字符", false);
+  }).on("blur", function () {
+    if (goodCode.val() != null && goodCode.val().length > 1 && goodCode.val().length < 10) {
+      validateTip(goodCode.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(goodCode.next(), {"color": "red"}, imgNo + " 商品编号输入的不符合规范，请重新输入", false);
+    }
+  });
+  
+  inventory.on("focus", function () {
+    validateTip(inventory.next(), {"color": "#666666"}, "* 请输入库存，必须为0或正整数", false);
+  }).on("blur", function () {
+    var patrn = /^0|\d+$/;
+    if (inventory.val().match(patrn)) {
+      validateTip(inventory.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(inventory.next(), {"color": "red"}, imgNo + " 您输入的库存不正确", false);
+    }
+  });
+  
+  goodType.on("focus", function () {
+    validateTip(goodType.next(), {"color": "#666666"}, "* 请选择商品类型", false);
+  }).on("blur", function () {
+    if (goodType.val() != null && goodType.val() > 0) {
+      validateTip(goodType.next(), {"color": "green"}, imgYes, true);
+    } else {
+      validateTip(goodType.next(), {"color": "red"}, imgNo + " 请重新选择商品类型", false);
+    }
+    
+  });
+  
+  saveBtn.on("click", function () {
+    goodName.blur();
+    inventory.blur();
+    goodCode.blur();
+    goodType.blur();
+    if (goodName.attr("validateStatus") == "true"
+        && inventory.attr("validateStatus") == "true"
+        && goodCode.attr("validateStatus") == "true"
+        && goodType.attr("validateStatus") == "true") {
+      if (confirm("是否确认要提交数据？")) {
+        $("#goodForm").submit();
+      }
+    }
+  });
+  
+  backBtn.on("click", function () {
+    //alert("modify: "+referer);
+    if (referer != undefined
+        && null != referer
+        && "" != referer
+        && "null" != referer
+        && referer.length > 4) {
+      window.location.href = referer;
+    } else {
+      history.back(-1);
+    }
+  });
+});
---
+++ src/main/webapp/jsp/goodmodify.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<%@include file="/jsp/common/head.jsp" %>
+<div class="right">
+    <div class="location">
+        <strong>你现在所在的位置是:</strong>
+        <span>商品管理页面 >> 商品修改页面</span>
+    </div>
+    <div class="providerAdd">
+        <form id="goodForm" name="goodForm" method="post" action="${pageContext.request.contextPath }/jsp/good.do">
+            <input type="hidden" name="method" value="modifyexe">
+            <input type="hidden" name="gid" value="${good.id }"/>
+            <div>
+                <label for="goodName">商品名称：</label>
+                <input type="text" name="goodName" id="goodName" value="${good.goodName }">
+                <font color="red"></font>
+            </div>
+            <div>
+                <label for="goodCode">商品编号：</label>
+                <input type="text" name="goodCode" id="goodCode" value="${good.goodCode}">
+                <font color="red"></font>
+            </div>
+            <div>
+                <label>商品类别：</label>
+                <input type="hidden" value="${good.goodType }" id="tid"/>
+                <select name="goodType" id="goodType"></select>
+            </div>
+            <div>
+                <label for="inventory">库存：</label>
+                <input type="text" name="inventory" id="inventory" value="${good.inventory }">
+                <font color="red"></font>
+            </div>
+            <div>
+                <label for="ownerName">所有者：</label>
+                <input type="text" name="ownerName" id="ownerName" value="${good.ownerName }">
+            </div>
+            <div class="providerAddBtn">
+                <input type="button" name="save" id="save" value="保存"/>
+                <input type="button" id="back" name="back" value="返回"/>
+            </div>
+        </form>
+    </div>
+</div>
+</section>
+<%@include file="/jsp/common/foot.jsp" %>
+<script type="text/javascript" src="${pageContext.request.contextPath }/js/goodmodify.js"></script>
