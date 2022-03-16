fc6f451a380748191397de4eafcdb4dc5fd66974
SaarChaffee <1240148332@qq.com>
2022-03-16 22:44:03 +0800

    add notes

--- src/main/java/com/chaffee/dao/bill/BillDao.java
+++ src/main/java/com/chaffee/dao/bill/BillDao.java
@@ -13,18 +13,72 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface BillDao {
-  //获取订单列表
+  /**
+   * 获取订单列表
+   *
+   * @param connection
+   * @param goodName
+   * @param customerName
+   * @param paymentMethod
+   * @param currentPageNo
+   * @param pageSize
+   * @return List<Bill>
+   * @throws SQLException
+   */
   public List<Bill> getBillList( Connection connection, String goodName, String customerName, int paymentMethod,
                                  int currentPageNo,
                                  int pageSize ) throws SQLException;
   
+  /**
+   * 获取订单数
+   *
+   * @param connection
+   * @param goodName
+   * @param customerName
+   * @param paymentMethod
+   * @return
+   * @throws SQLException
+   */
   public int getBillCount( Connection connection, String goodName, String customerName, int paymentMethod ) throws SQLException;
   
+  /**
+   * 更新订单
+   *
+   * @param connection
+   * @param id
+   * @param bill
+   * @return
+   * @throws SQLException
+   */
   public int updateBill( Connection connection, int id, Bill bill ) throws SQLException;
   
+  /**
+   * 通过id获得订单
+   *
+   * @param connection
+   * @param id
+   * @return
+   * @throws SQLException
+   */
   public Bill getBillById( Connection connection, int id ) throws SQLException;
   
+  /**
+   * 增加订单
+   *
+   * @param connection
+   * @param bill
+   * @return
+   * @throws SQLException
+   */
   public int addBill( Connection connection, Bill bill ) throws SQLException;
   
+  /**
+   * 删除订单
+   *
+   * @param connection
+   * @param id
+   * @return
+   * @throws SQLException
+   */
   public int deleteBill( Connection connection, int id ) throws SQLException;
 }
--- src/main/java/com/chaffee/dao/good/GoodDao.java
+++ src/main/java/com/chaffee/dao/good/GoodDao.java
@@ -13,21 +13,92 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface GoodDao {
+  /**
+   * 获得商品列表
+   *
+   * @param connection
+   * @param goodName
+   * @param ownerName
+   * @param goodType
+   * @param currentPageNo
+   * @param pageSize
+   * @return List<Good>
+   * @throws SQLException
+   */
   public List<Good> getGoodList( Connection connection, String goodName, String ownerName, int goodType,
                                  int currentPageNo,
                                  int pageSize ) throws SQLException;
   
+  /**
+   * 获得商品数量
+   *
+   * @param connection
+   * @param goodName
+   * @param ownerName
+   * @param goodType
+   * @return
+   * @throws SQLException
+   */
   public int getGoodCount( Connection connection, String goodName, String ownerName, int goodType ) throws SQLException;
   
+  /**
+   * 更新商品
+   *
+   * @param connection
+   * @param id
+   * @param good
+   * @return
+   * @throws SQLException
+   */
   public int updateGood( Connection connection, int id, Good good ) throws SQLException;
   
+  /**
+   * 通过id获得商品
+   *
+   * @param connection
+   * @param id
+   * @return
+   * @throws SQLException
+   */
   public Good getGoodById( Connection connection, int id ) throws SQLException;
   
+  /**
+   * 通过商品名获得商品
+   *
+   * @param connection
+   * @param goodName
+   * @return
+   * @throws SQLException
+   */
   public Good getGoodByName( Connection connection, String goodName ) throws SQLException;
   
+  /**
+   * 增加商品
+   *
+   * @param connection
+   * @param good
+   * @return
+   * @throws SQLException
+   */
   public int addGood( Connection connection, Good good ) throws SQLException;
   
+  /**
+   * 删除商品
+   *
+   * @param connection
+   * @param id
+   * @return
+   * @throws SQLException
+   */
   public int deleteGood( Connection connection, int id ) throws SQLException;
   
+  /**
+   * 通过商品编号获得商品
+   *
+   * @param connection
+   * @param code
+   * @return
+   * @throws SQLException
+   */
   public Good getGoodByCode( Connection connection, String code ) throws SQLException;
 }
